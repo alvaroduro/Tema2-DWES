2669f847ed6040596466c6a37677e72ff677bed7
AlvaroDuran <adurama436@g.educaand.es>
2024-12-02 16:57:32 +0100

    adduser.php

--- Usuarios/adduser.php
+++ Usuarios/adduser.php
@@ -1,10 +1,50 @@
+<?php
+//Añadir Usuarios
+//Requerimos conexión
+require_once 'config.php';
+
+//Mensaje de confirmación
+$msgresultado = "";
+
+//Si se pulsa en Guardar
+if (isset($_POST['guardar'])) {
+    //Vemos que los campos no están vacíos
+    if (isset($_POST) && (!empty($_POST))) {
+        $nombre = $_POST['txtnombre'];
+        $password = sha1($_POST['txtpass']);
+        $email = $_POST['txtemail'];
+
+        //Definimos la instruccion sql
+        try {
+            $sql = "INSERT INTO usuario(nombre, password, email) VALUES (:nombre,:password,:email)";
+
+            //Preparamos la consulta
+            $query = $conexion->prepare($sql);
+
+            //Ejecutamos indicando los valores de los parámetros
+            $query->execute(['nombre' => $nombre, 'password' => $password, 'email' => $email]);
+
+            //Comprobamos que se ha realizado correctamente
+            if ($query) {
+                $msgresultado = '<div class="alert alert-success mx-2">' . "El usuario se registró correctamente!!" . '<img width="50" height="50" src="https://img.icons8.com/clouds/100/ok-hand.png" alt="ok-hand"/></div>';
+            } // o no
+
+        } catch (PDOException $ex) {
+            $msgresultado = '<div class="alert alert-danger w-100 mx-2">' . "Fallo al insertar usuario a la Base de Datos!!" . '<img class="mx-2" width="40" height="40" src="https://img.icons8.com/cute-clipart/64/error.png" alt="error"/></div>';
+            //die();
+        }
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Base de Datos con PHP y PDO</title>
+    <title>Añadir Usuario</title>
     <!--CSS-->
     <link rel="stylesheet" href="estilos.css">
     <!--GoogleFont-->
@@ -25,7 +65,7 @@
 </head>
 
 <body>
-    <!--Navegación-->
+    <!--NAVEGACION-->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
             <a class="navbar-brand" href="../index.php">INICIO</a>
@@ -47,8 +87,13 @@
             </div>
         </div>
     </nav>
+
     <!--TITULO PRINCIPAL-->
-    <h1 class="text-center my-3">CRUD USUARIOS Apuntes</h1>
+    <div class="container d-flex justify-content-between my-3">
+        <h1 class="text-center">Añadir Usuarios</h1>
+        <a class="navbar-brand fs-5" href="index.php"><img class="mx-2" width="30" height="30" src="https://img.icons8.com/flat-round/50/back--v1.png" alt="back--v1" />Atrás</a>
+    </div>
+
     <!--PRINCIPAL-->
     <div class="container">
         <!--Titulo-->
@@ -58,18 +103,24 @@
             <h2>Base de Datos de Usuarios</h2>
             </p>
         </div><!--Fin Titulo-->
-        <!--Enlaces-->
-        <div class="container my-3 enlacesPrincipal">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link" href="listarUsuarios.php"><img class="mx-2" width="80" height="80" src="https://img.icons8.com/clouds/100/right.png" alt="right" />Listar Usuarios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="adduser.php"><img class="mx-2" width="80" height="80" src="https://img.icons8.com/bubbles/100/right.png" alt="right" />Añadir Usuario</a>
-                </li>
-            </ul>
+
+        <!--Mostramos si se inserto correctamente-->
+        <?php echo $msgresultado ?>
+
+        <!--Formulario-->
+        <div class="formulario my-2">
+            <form action="adduser.php" method="post">
+                <label for="txtnombre">Nombre</label>
+                <input type="text" class="form-control" name="txtnombre" required></br>
+                <label for="txtemail">Email</label>
+                <input type="text" class="form-control" name="txtemail"></br>
+                <label for="txtpass">Password</label>
+                <input type="text" class="form-control" name="txtpass" required></br>
+                <input type="submit" value="Guardar" class="btn btn-success" name="guardar">
+            </form>
         </div>
     </div>
+
     <!--JS BOOSTRAP-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
--- Usuarios/listarUsuarios.php
+++ Usuarios/listarUsuarios.php
@@ -70,10 +70,10 @@
 
         //Si hay datos en la consulta
         if ($resultado) {
-            $msgresultado = '<div class="alert alert-success">' . "La consulta se realizó correctamente!!" . '<img width="50" height="50" src="https://img.icons8.com/clouds/100/ok-hand.png" alt="ok-hand"/></div>';
+            $msgresultado = '<div class="alert alert-success mx-2">' . "La consulta se realizó correctamente!!" . '<img width="50" height="50" src="https://img.icons8.com/clouds/100/ok-hand.png" alt="ok-hand"/></div>';
         } //o no
     } catch (PDOException $ex) {
-        $msgresultado = '<div class="alert alert-success w-100">' . "Fallo al realizar al consulta a la Base de Datos!!" . '<img class="mx-2" width="40" height="40" src="https://img.icons8.com/cute-clipart/64/error.png" alt="error"/></div>';
+        $msgresultado = '<div class="alert alert-danger w-100 mx-2">' . "Fallo al realizar al consulta a la Base de Datos!!" . '<img class="mx-2" width="40" height="40" src="https://img.icons8.com/cute-clipart/64/error.png" alt="error"/></div>';
         //die();
     }
 
@@ -92,8 +92,8 @@
     <!--Mostramos mensaje sobre la consulta-->
     <?php echo "</br>" . $msgresultado; ?>
     <!--Creamos tabla para la -->
-    <table class="table table-striped">
-    <th colspan="2" class="text-center fs-3">TABLA USUARIOS</th>
+    <table class="table table-striped tablaListar w-75">
+        <th colspan="2" class="text-center fs-3">TABLA USUARIOS</th>
         <!--Fila-->
         <tr>
             <!--Columnas Encabezados-->
